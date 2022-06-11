a49c9677bf2da48a4e9e028ee95b170dea119d5d
Jongwon Park <pjw970220@gmail.com>
2022-06-12 02:52:08 +0900

    init

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -1,21 +1,35 @@
-//
-//  ContentView.swift
-//  Shared
-//
-//  Created by Mone on 2022/06/12.
-//
-
 import SwiftUI
+import PencilKit
 
 struct ContentView: View {
+    @State var canvasView = PKCanvasView()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Canvas(canvasView: canvasView)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct Canvas: UIViewRepresentable {
+    var canvasView: PKCanvasView
+    let picker = PKToolPicker.init()
+    
+    func makeUIView(context: Context) -> PKCanvasView {
+        self.canvasView.tool = PKInkingTool(.pen, color: .black, width: 15)
+        self.canvasView.becomeFirstResponder()
+        return canvasView
+    }
+    
+    func updateUIView(_ uiView: PKCanvasView, context: Context) {
+        picker.addObserver(canvasView)
+        picker.setVisible(true, forFirstResponder: uiView)
+        DispatchQueue.main.async {
+            uiView.becomeFirstResponder()
+        }
     }
 }
