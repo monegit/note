afb38a2e65b04051da1da6d9d14094f583243858
Jongwon Park <pjw970220@gmail.com>
2022-06-12 00:20:22 +0900

    Initial Commit

---
+++ Shared/noteApp.swift
@@ -0,0 +1,17 @@
+//
+//  noteApp.swift
+//  Shared
+//
+//  Created by Mone on 2022/06/12.
+//
+
+import SwiftUI
+
+@main
+struct noteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
